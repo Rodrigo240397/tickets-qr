69bbdb9574edd9646301f61b91f2715b16c4276a
Rodrigo <rodrigo.figueroa.pacheco@gmail.com>
2026-05-28 11:18:09 +0200

    añadir ruta para listar todos los tickets

--- routes/tickets-routes.js
+++ routes/tickets-routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express"
+import pool from "../db/connection.js"
+import TicketRepository from "../models/TicketRepository.js"
+import TicketController from "../controllers/TicketController.js"
 
 const router = Router()
+const ticketRepository = new TicketRepository(pool)
+const ticketController = new TicketController(ticketRepository)
 
 router.get("/", (req, res) => {
   res.render("index", { title: "Inicio" })
 })
+
+router.get("/tickets", (req, res) => ticketController.getTickets(req, res))
 
 export default router
