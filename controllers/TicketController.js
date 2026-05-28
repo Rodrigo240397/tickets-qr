bc93dfd3657f86b73306f243ade6234340d72ee4
Rodrigo <rodrigo.figueroa.pacheco@gmail.com>
2026-05-28 11:07:28 +0200

    añadir controlador de tickets con metodo para listar los tickets disponibles

---
+++ controllers/TicketController.js
@@ -0,0 +1,15 @@
+export default class TicketController {
+  constructor(ticketRepository) {
+    this.ticketRepository = ticketRepository;
+  }
+
+  async getTickets(req, res) {
+    try {
+      const tickets = await this.ticketRepository.getTickets();
+      res.render("tickets", { title: "Tickets", tickets });
+    } catch (error) {
+      console.error("Error al obtener tickets:", error);
+      res.status(500).json({ error: "No se pudieron obtener los tickets" });
+    }
+  }
+}
