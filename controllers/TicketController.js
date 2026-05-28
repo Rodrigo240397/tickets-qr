0b8fa2fb3f5a97eb29b130529026c0b2d6781ebc
Rodrigo <rodrigo.figueroa.pacheco@gmail.com>
2026-05-28 13:00:44 +0200

    añadir una nueva ruta para mostrar un ticket

--- controllers/TicketController.js
+++ controllers/TicketController.js
@@ -26,12 +26,26 @@ export default class TicketController {
       ticket.generarToken();
       await this.ticketRepository.createTicket(ticket);
       //res.status(201).redirect("/tickets");
-      const qr = await ticket.generarQR();
-      res.render('tickets/mostrar-ticket', { title: "Ticket", ticket,qr })
+      res.redirect(`/tickets/${ticket.getToken()}`);
 
     } catch (error) {
       console.error("Error al crear ticket:", error);
       res.status(500).json({ error: "No se pudo crear el ticket" });
+    }
+  }
+
+  async mostrarTicket(req, res) {
+    try {
+      const { token } = req.params;
+      const ticket = await this.ticketRepository.getTicketByToken(token);
+      if (!ticket) {
+        return res.status(404).json({ error: "Ticket no encontrado" });
+      }
+      const qr = await ticket.generarQR();
+      res.render('tickets/mostrar-ticket', { title: "Ticket", ticket, qr });
+    } catch (error) {
+      console.error("Error al mostrar ticket:", error);
+      res.status(500).json({ error: "No se pudo mostrar el ticket" });
     }
   }
 }
